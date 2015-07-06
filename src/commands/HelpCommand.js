52ff621213d77376366cfdf8a71ee7102a9a7ec0
Arne Van Raepenbusch <arvraepe@gmail.com>
2015-07-06 16:44:14 +0200

    updated help

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -4,5 +4,6 @@ exports.execute = function (argv) {
     console.log('Welcome! The help is here for you!');
     console.log('wmg new'.green, ' allows you to create a new account in wmg');
     console.log('wmg login'.green, ' allows you to login to an existing account in wmg');
+    console.log('wmg whoami'.green, ' shows you the current logged in user');
     process.exit(0);
 };
